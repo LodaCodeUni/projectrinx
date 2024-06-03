e35fbf2397fc5d90a5138ea648d97273d4c953a0
Шило Артём <ArSh@gendalf.ru>
2024-06-03 23:27:01 +0300

    first

---
+++ LABRAB/Helper/FindPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LABRAB.Model;
+
+namespace LABRAB.Helper
+{
+    internal class FindPerson
+    {
+        private int id;
+
+        public FindPerson(int id)
+        {
+            this.id = id;
+        }
+
+        public bool PersonPredicate(Person person)
+        {
+            return person.Id == id;
+        }
+    }
+}
---
+++ LABRAB/Helper/FindRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LABRAB.Model;
+
+namespace LABRAB.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+
+}
